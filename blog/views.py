92e70b6d7637b4d9f70c28a668871b9752b62e56
Kim Dayoung <dayoung8893@naver.com>
2023-06-28 13:56:49 +0900

    no message

--- blog/views.py
+++ blog/views.py
@@ -7,25 +7,26 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Post
 from rest_framework.views import APIView
-from rest_framework import generics, status
+from rest_framework import status
 
 from .serializer import PostSerializer
 
+# @api_view
+# class PostListAPI(APIView):
+#     def get(self, request):
+#         queryset = Post.objects.all()
+#         print(queryset)
+#         serializer = PostSerializer(queryset, many=True)
+#         return Response(serializer.data)
 
-class PostListAPI(APIView):
-    def get(self, request):
-        queryset = Post.objects.all()
-        print(queryset)
-        serializer = PostSerializer(queryset, many=True)
-        return Response(serializer.data)
-
-@api_view(['POST'])
-def PostDetail(request):
-    # reqId = request.id
-    # reqPw = request.pw
-    reqData = request.data
-    serializer = PostSerializer(data=reqData)
-
-    serializer.save() # 데이터베이스에 테이블 저장
-
-    return Response(serializer.data, status=status.HTTP_201_CREATED)
+# @api_view(['POST'])
+# def PostDetail(request):
+#     # reqId = request.id
+#     # reqPw = request.pw
+#     reqData = request.data
+#     serializer = PostSerializer(data=reqData)
+#
+#     if serializer.is_valid(raise_exception=True):
+#         serializer.save() # 데이터베이스에 테이블 저장
+#
+#         return Response(serializer.data, status=status.HTTP_201_CREATED)
