c0b9b7e675121923653548539b68a3fc2a21947e
Kim Dayoung <dayoung8893@naver.com>
2023-06-28 17:19:40 +0900

    no message

--- blog/models.py
+++ blog/models.py
@@ -17,10 +17,3 @@ class Post(models.Model):
     # created_date = models.DateTimeField(
     #         default=timezone.now)
     # published_date = models.DateTimeField(default=timezone.now, blank=True)
-
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
-
-    def __str__(self):
-        return self.id
