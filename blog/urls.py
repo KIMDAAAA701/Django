473ea18089dfe2e7b406d1fdf39b30a66001ea48
Kim Dayoung <dayoung8893@naver.com>
2023-06-28 14:29:41 +0900

    no message

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ from django.urls import include, path
 from .views import PostListAPI, PostDetail
 
 router = routers.DefaultRouter() #DefaultRouter를 설정
-router.register('post', PostListAPI.as_view(), basename = 'post') #itemviewset 과 item이라는 router 등록
+router.register('post/', PostListAPI.as_view(), basename = 'post') #itemviewset 과 item이라는 router 등록
 
 urlpatterns = [
     path('', PostListAPI.as_view(), name='post'),
