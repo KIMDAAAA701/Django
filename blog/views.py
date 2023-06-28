bcd42bc3aefef46c6eba2425046f82d1441fb8d4
Kim Dayoung <dayoung8893@naver.com>
2023-06-28 21:01:13 +0900

    no message

--- blog/views.py
+++ blog/views.py
@@ -25,23 +25,24 @@ def post_create(request):
     # reqId = request.id
     # reqPw = request.pw
     reqData = request.data
-    # reqData['key'] = int(timezone.now().timestamp())
+    reqData['key'] = float(timezone.now().timestamp())
     serializer = PostSerializer(data=reqData)
 
     if serializer.is_valid(raise_exception=True):
 
         # 중복 검사 로직 추가
         id_value =serializer.validated_data.get('ID')
+        pw_value = serializer.validated_data.get('PW')
+        key_value = serializer.validated_data.get('key')
         if Post.objects.filter(id_value=id_value).exists():
             return Response({'error': 'Duplicate ID value'}, status=status.HTTP_400_BAD_REQUEST)
-        pw_value = serializer.validated_data.get('PW')
-        if Post.objects.filter(pw_value=pw_value).exists():
+        elif Post.objects.filter(pw_value=pw_value).exists():
             return Response({'error': 'Duplicate PW value'}, status=status.HTTP_400_BAD_REQUEST)
-        key_value = serializer.validated_data.get('key')
-        if Post.objects.filter(key_value=key_value).exists():
-            return Response({'error': 'Duplicate key value'}, status=status.HTTP_400_BAD_REQUEST)
 
-        serializer.save()  # 데이터베이스에 테이블 저장
+        elif Post.objects.filter(key_value=key_value).exists():
+            return Response({'error': 'Duplicate key value'}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            serializer.save()  # 데이터베이스에 테이블 저장
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
